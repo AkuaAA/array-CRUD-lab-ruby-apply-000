0ed8390644feaae99c9a776d15c9cae3067e8bf0
Akua Apeagyei <apeagyei.akua@gmail.com>
2018-08-14 21:45:51 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,8 +18,8 @@ def remove_element_from_end_of_array(wow)
 wow.pop
 end
 
-def remove_element_from_start_of_array
-
+def remove_element_from_start_of_array(wow)
+wow.shift
 end
 
 def retrieve_element_from_index
